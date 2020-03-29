0221677ae2be64cabbbf037b2e811d3db383a1d5
Marcin Wisniewski <mwisniewski@utilitywarehouse.co.uk>
2020-03-29 13:11:45 +0100

    fixes changing battery index

--- scripts/battery_level.sh
+++ scripts/battery_level.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-acpi | grep "Battery 0" | awk '{ gsub(",","",$4); gsub("%","",$4); print $4}'
+for i in $(acpi | grep "Battery" | awk '{ gsub(",","",$4); gsub("%","",$4); print $4}')
+do
+    if [ "$i" != "" ]
+    then
+        echo "$i"
+        break
+    fi
+done
